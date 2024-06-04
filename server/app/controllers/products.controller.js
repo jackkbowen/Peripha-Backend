a3fd0b39a18cee7bb07d1d37eeaa71d8ec4a7602
jackkbowen <jmbowen@ucsc.edu>
2024-06-03 18:45:36 -0700

    Modifying style guide

--- server/app/controllers/products.controller.js
+++ server/app/controllers/products.controller.js
@@ -1,134 +1,147 @@
-const Products = require("../models/products.model");
-const Users = require("../models/users.model");
-const asyncHandler = require('express-async-handler')
+const Products = require('../models/products.model');
+const Users = require('../models/users.model');
+const asyncHandler = require('express-async-handler');
 const mongoose = require('mongoose');
 
-exports.create = asyncHandler(async(req, res) => {
-    // Validate request
-    if (!req.body.name) {
-        res.status(400).send({message: "Name field cannot be empty"})
-        return;
-    } else if (!req.body.category) {
-        res.status(400).send({message: "Category field cannot be empty"})
-        return;
-    } 
+exports.create = asyncHandler(async (req, res) => {
+  // Validate request
+  if (!req.body.name) {
+    res.status(400).send({ message: 'Name field cannot be empty' });
+    return;
+  }
+  if (!req.body.category) {
+    res.status(400).send({ message: 'Category field cannot be empty' });
+    return;
+  }
 
-    // Check for existing Product
-    const productExists = await Products.findOne({ name: req.body.name });
-        if (productExists) {
-            res.status(409).send({message: "ERROR: Product already exists. Please use the existing product.", 
-                                product: req.body.name});
-            return;
-        }
-
-    // Create a new Product
-    const product = new Products({
-        name: req.body.name,
-        category: req.body.category,
-        manufacturer: req.body.manufacturer,
-        model: req.body.model,
-        image: req.body.image,
-        reviews: [],
-        specs: req.body.specs
+  // Check for existing Product
+  const productExists = await Products.findOne({ name: req.body.name });
+  if (productExists) {
+    res.status(409).send({
+      message: 'ERROR: Product already exists. Please use the existing product.',
+      product: req.body.name
     });
+    return;
+  }
 
-    // Save Product in the database
-    product
-        .save(product)
-        .then(data => {
-            res.status(200).send(data);
-            return;
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Some error occurred while creating the Product."
-            });
-            return;
-        });
+  // Create a new Product
+  const product = new Products({
+    name: req.body.name,
+    category: req.body.category,
+    manufacturer: req.body.manufacturer,
+    model: req.body.model,
+    image: req.body.image,
+    reviews: [],
+    specs: req.body.specs
+  });
+
+  // Save Product in the database
+  product
+    .save(product)
+    .then((data) => {
+      res.status(200).send(data);
+      return;
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Some error occurred while creating the Product.'
+      });
+      return;
+    });
 });
 
 // Find a single product with an id
-exports.findOne = asyncHandler(async(req, res) => {
-    const id = req.params.productId;
+exports.findOne = asyncHandler(async (req, res) => {
+  const id = req.params.productId;
 
-    Products.findById(id)
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ 
-                    message: "No Products found with id " + id });
-                return;
-            }
-            else res.status(200).send(data);;
-            return;
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Product with id=" + id });
-            return;
+  Products.findById(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: 'No Products found with id ' + id
         });
+        return;
+      }
+      res.status(200).send(data);
+      return;
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error retrieving Product with id=' + id
+      });
+      return;
+    });
 });
 
-exports.findUserProducts = asyncHandler(async(req, res) => {
-    const username = req.params.username;
-    let products = [];
-    await Users.findOne({username: username})
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ 
-                    message: "Not found Users with id " + username });
-                return;
-            }
-            products = data.products;
-            Products.find({
-                '_id': {
-                    $in : products
-                }
-            }).then(data => {
-                res.status(200).send(data);
-                return;
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: "Error retrieving Products from user " + username });
-                return;
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Users with username=" + username });
-            return;
+exports.findUserProducts = asyncHandler(async (req, res) => {
+  const username = req.params.username;
+  let products = [];
+  await Users.findOne({ username: username })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: 'Not found Users with id ' + username
         });
+        return;
+      }
+      products = data.products;
+      Products.find({
+        _id: {
+          $in: products
+        }
+      })
+        .then((data) => {
+          res.status(200).send(data);
+          return;
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: 'Error retrieving Products from user ' + username
+          });
+          return;
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error retrieving Users with username=' + username
+      });
+      return;
+    });
 });
 
-exports.searchProductsDB = asyncHandler(async(req, res) => {
-    const queryString = req.body.queryString;
-    const filters = req.body.filters;
-    if (!filters) {
-        await Products.find({name: {$regex: queryString}})
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ 
-                    message: "No products found matching: " + queryString });
-                return;
+exports.searchProductsDB = asyncHandler(async (req, res) => {
+  const queryString = req.body.queryString;
+  const filters = req.body.filters;
+  if (!filters) {
+    await Products.find({ name: { $regex: queryString } })
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            message: 'No products found matching: ' + queryString
+          });
+          return;
+        }
+        extractedData = Products.find(
+          {
+            _id: {
+              $in: data
             }
-            extractedData = Products.find({
-                '_id': {
-                    $in : data
-                }, 
-                },
-                { _id: 1, name: 1, category: 1, image: 1 }
-            ).sort({ name : 1 })
-            .then(data => {
-                res.status(200).send(data);
-                return;
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: "Error retrieving Products from user " + username });
-                return;
+          },
+          { _id: 1, name: 1, category: 1, image: 1 }
+        )
+          .sort({ name: 1 })
+          .then((data) => {
+            res.status(200).send(data);
+            return;
+          })
+          .catch((err) => {
+            res.status(500).send({
+              message: 'Error retrieving Products from user ' + username
             });
-        });
-    } else {
-        // todo apply filters
-    }
-})
+            return;
+          });
+      });
+  } else {
+    // todo apply filters
+  }
+});
--- server/app/utils/password.js
+++ server/app/utils/password.js
@@ -1,119 +1,116 @@
 const crypto = require('crypto');
 const mongoose = require('mongoose');
-const User = require("../models/users.model");
-const Blacklist = require("../models/blacklist.model");
-const Reviews = require("../models/reviews.model");
-const jwt = require("jsonwebtoken"); 
-const userController = require("../controllers/users.controller.js");
+const User = require('../models/users.model');
+const Blacklist = require('../models/blacklist.model');
+const Reviews = require('../models/reviews.model');
+const jwt = require('jsonwebtoken');
+const userController = require('../controllers/users.controller.js');
 
 // Function to generate a hashed password and salt
 // Used in account creation to encode password
 function genPassword(password) {
-    const salt = crypto.randomBytes(32).toString('hex');
-    const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex');
-    return {
-      salt: salt,
-      hash: genHash
-    };
+  const salt = crypto.randomBytes(32).toString('hex');
+  const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex');
+  return {
+    salt: salt,
+    hash: genHash
+  };
 }
 
 // Function to validate if the provided password matches the stored hash and salt
 function validPassword(password, hash, salt) {
-    var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex');
-    return hash === hashVerify;
+  const hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex');
+  return hash === hashVerify;
 }
 
 // Verify a username is valid and check if the password matches the encrypted salt/hash
-function verifyUser (username, password) {
-    const user = User.findOne({username: username})
+async function verifyUser(username, password) {
+  const user = await User.findOne({ username: username });
 
-    // No user registered with inputted email.
-    if (!user) { 
-        return false
-    }
-    
-    const isValid = validPassword(password, user.hash, user.salt);
-    // Successful login.
-    if (isValid) {
-        user.save();
-        return true;
+  // No user registered with inputted email.
+  if (!user) {
+    return false;
+  }
+
+  const isValid = validPassword(password, user.hash, user.salt);
+  // Successful login.
+  if (isValid) {
+    await user.save();
+    return true;
 
     // Failed login.
-    } else { 
-        console.log('Invalid username or password');
-        return false;
-    }
-  
-};
+  } else {
+    console.log('Invalid username or password');
+    return false;
+  }
+}
 
 // Verifies JWT of the token passed in authToken
 // Returns the username of the user with the corresponding token
 async function verifyJWT(authToken, req, res, done) {
+  // Verifies an authToken exists
+  // authToken is extracted token from header
+  if (!authToken) {
+    res.status(403).send({ message: 'No Auth Token - You are not authorized to access this page. Please log in.' });
+    return done();
+  }
 
-    // Verifies an authToken exists
-    // authToken is extracted token from header
-    if (!authToken) {
-        res.status(403).send({ message: 'No Auth Token - You are not authorized to access this page. Please log in.' });
-        done();
-    } else {
-        try {
-            // Check if the token is in the blacklist
-            req.params.tokenInBlacklist = await Blacklist.findOne({ token: authToken });
-            if (req.params.tokenInBlacklist) {
-                res.status(403).send({ message: 'Token is blacklisted. Please log in again.' });
-                return done();
-            }
-
-            // Decode a user from the JWT using the authToken
-            const user = await new Promise((resolve, reject) => {
-                jwt.verify(authToken, 'secret', (err, decoded) => {
-                    if (err) {
-                        reject(err);
-                        done();
-                    } else {
-                        resolve(decoded);
-                    }
-                });
-            });
-
-            // Get user info from ID returned by decoding JWT
-            userData = await User.findById(user.id);
-            if (!userData){
-                res.status(403).send({ message: "No ID found for the JWT token. Please log in again." });
-                done();
-            }
-            return userData.username
-        } catch (err) {
-            console.log(err);
-            res.status(403).send( { message: 'Verify JWT Failure', error: err } );
-            done(); 
-        }
+  try {
+    // Check if the token is in the blacklist
+    req.params.tokenInBlacklist = await Blacklist.findOne({ token: authToken });
+    if (req.params.tokenInBlacklist) {
+      res.status(403).send({ message: 'Token is blacklisted. Please log in again.' });
+      return done();
     }
+
+    // Decode a user from the JWT using the authToken
+    const user = await new Promise((resolve, reject) => {
+      jwt.verify(authToken, 'secret', (err, decoded) => {
+        if (err) {
+          reject(err);
+          done();
+        } else {
+          resolve(decoded);
+        }
+      });
+    });
+
+    // Get user info from ID returned by decoding JWT
+    const userData = await User.findById(user.id);
+    if (!userData) {
+      res.status(403).send({ message: 'No ID found for the JWT token. Please log in again.' });
+      return done();
+    }
+    return userData.username;
+  } catch (err) {
+    console.log(err);
+    res.status(403).send({ message: 'Verify JWT Failure', error: err });
+    done();
+  }
 }
 
 // General Auth function to check if there is ANY user signed in
-// Used for logout and other functions that dont require a protected route for a specific user
+// Used for logout and other functions that don't require a protected route for a specific user
 function verifyUserAccessAnyUser(req, res, done) {
-    // Extract the JWT token
-    // May have "Bearer" in front of token based on how req is sent, need to split
-    const token = req.headers["authorization"];
+  // Extract the JWT token
+  // May have "Bearer" in front of token based on how req is sent, need to split
+  const token = req.headers['authorization'];
 
-    // Decode the JWT token passed in the req.headers["authorization"]
-    verifyJWT(token, req, res, done).then(async(username) => {
-       
-        // Set the username parameter as the username decoded from verify
-        // Makes easy use of the decoded username in subsequent requests
-        req.params.username = username
+  // Decode the JWT token passed in the req.headers['authorization']
+  verifyJWT(token, req, res, done).then(async (username) => {
+    // Set the username parameter as the username decoded from verify
+    // Makes easy use of the decoded username in subsequent requests
+    req.params.username = username;
 
-        if (username) {
-            // If a username exists, continue
-            done ();
-        }
-        // Error occurs if there is no username associated with the JWT 
-    }).catch(err => {
-            console.log(err);
-            res.status(500).send( {message: "Some error occurered in verifyUserAccessAnyUser. Please try again."} );
-        });
+    if (username) {
+      // If a username exists, continue
+      done();
+    }
+    // Error occurs if there is no username associated with the JWT
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).send({ message: 'Some error occurred in verifyUserAccessAnyUser. Please try again.' });
+  });
 }
 
 // More specific auth function to verify the user is making a request to a page they own
@@ -121,39 +118,35 @@ function verifyUserAccessAnyUser(req, res, done) {
 // Fills the decoded username in the params in this case
 // Returns the user info that was decoded
 function verifyUserAccess(req, res, done) {
-    const authToken = req.headers["authorization"];
+  const authToken = req.headers['authorization'];
 
-    verifyJWT(token, req, res, done).then(async(username) => {
+  verifyJWT(authToken, req, res, done).then(async (username) => {
+    // Query to get the username from review (if applicable)
+    // Maybe should move to reviews function
+    if ((!req.body.username) && (!req.params.username)) {
+      const reviewId = mongoose.Types.ObjectId(req.params.reviewId);
+      const userData = await Reviews.findById(reviewId).select('username');
 
-        // Query to get the username from review (if applicable) 
-        // Maybe should move to reviews function
-        if ((!req.body.username) && (!req.params.username)) {
-            const reviewId = mongoose.Types.ObjectId(req.params.reviewId);
-            userData = await Reviews.findById(reviewId).select('username');
-            
-            // Store username in params if userData found
-            if (userData) {
-                req.params.username = userData.username;
-            }
-        }
-        
-        // Set comparisonUsername to whichever is not unidentified
-        var comparisonUsername = req.params.username ?? req.body.username;
+      // Store username in params if userData found
+      if (userData) {
+        req.params.username = userData.username;
+      }
+    }
 
-        // If a username exists, continue if it matches comparisonUsername
-        // Matching JWT to request user
-        if (username) {
-            if (comparisonUsername == username) {
-                done();
-            }
-            else {
-                res.status(403).send( {message: "You cannot access accounts that are not yours. Please log in."} ) 
-            }
-        }
-    })
+    // Set comparisonUsername to whichever is not unidentified
+    const comparisonUsername = req.params.username ?? req.body.username;
+
+    // If a username exists, continue if it matches comparisonUsername
+    // Matching JWT to request user
+    if (username) {
+      if (comparisonUsername === username) {
+        done();
+      } else {
+        res.status(403).send({ message: 'You cannot access accounts that are not yours. Please log in.' });
+      }
+    }
+  });
 }
-
-
 
 module.exports.validPassword = validPassword;
 module.exports.genPassword = genPassword;
@@ -161,5 +154,3 @@ module.exports.verifyUser = verifyUser;
 module.exports.verifyUserAccess = verifyUserAccess;
 module.exports.verifyUserAccessAnyUser = verifyUserAccessAnyUser;
 module.exports.verifyJWT = verifyJWT;
-
-
